80a686c4f640e509e264b66011c3c636da0eb9b3
Uhrin Andor <andoruhrin@gmail.com>
2026-01-31 14:10:58 +0100

    fixed aura

--- RestifyClient/src/app/app.config.ts
+++ RestifyClient/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import Aura from "@primeuix/themes/Aura"
+import Aura from "@primeuix/themes/aura"
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
