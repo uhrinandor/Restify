8b807a06036d7afbb8a4128f9234df6a1ff36128
Uhrin Andor <andoruhrin@gmail.com>
2026-01-31 12:41:36 +0100

    added ui lib

--- RestifyClient/src/app/app.config.ts
+++ RestifyClient/src/app/app.config.ts
@@ -1,12 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import Aura from "@primeuix/themes/Aura"
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+      providePrimeNG({
+          theme: {
+              preset: Aura
+          }
+      })
   ]
 };
